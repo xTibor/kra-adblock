b793a1ef81e6ae90eb900181dfb7dbf98754af93
Nagy Tibor <xnagytibor@protonmail.com>
2023-08-18 16:14:27 +0200

    Update to Krita 5.2.0 beta2, reorganize block lists

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ A plugin for removing advertisements from Krita's home page.
 ## Supported versions
 
 - Krita 5.1.x
-- Krita 5.2 beta1
+- Krita 5.2 beta2
 - Krita 5.3 prealpha
 
 ## License
--- kra_adblock/kra_adblock.py
+++ kra_adblock/kra_adblock.py
@@ -1,68 +1,52 @@
 from krita import *
 import re
 
-widgetBlockList = {
-    '5.1': [
-        # Tested with: Krita 5.1.5
-        (QLabel,            'gettingStartedLink' ),
-        (QLabel,            'helpTitleLabel'     ),
-        (QLabel,            'kritaWebsiteLink'   ),
-        (QLabel,            'labelSupportText'   ),
-        (QLabel,            'manualLink'         ),
-        (QLabel,            'poweredByKDELink'   ),
-        (QLabel,            'sourceCodeLink'     ),
-        (QLabel,            'supportKritaLink'   ),
-        (QLabel,            'userCommunityLink'  ),
-        (QPushButton,       'gettingStartedIcon' ),
-        (QPushButton,       'kdeIcon'            ),
-        (QPushButton,       'kritaWebsiteIcon'   ),
-        (QPushButton,       'sourceCodeIcon'     ),
-        (QPushButton,       'supportKritaIcon'   ),
-        (QPushButton,       'userCommunityIcon'  ),
-        (QPushButton,       'userManualIcon'     ),
-        (QWidget,           'widgetRight'        ),
-    ],
-    '5.2': [
-        # Tested with: Krita 5.2.0 beta1
-        # https://invent.kde.org/graphics/krita/-/merge_requests/1853
-        (QLabel,            'labelSupportText'   ),
-        (QLabel,            'lblBanner'          ),
-        (QWidget,           'widgetCenter'       ),
-        (QWidget,           'widgetRight'        ),
-    ],
-    '5.3': [
-        # Tested with: Krita 5.3.0 prealpha
-        (QLabel,            'gettingStartedLink' ),
-        (QLabel,            'helpTitleLabel'     ),
-        (QLabel,            'kritaWebsiteLink'   ),
-        (QLabel,            'labelSupportText'   ),
-        (QLabel,            'lblBanner'          ),
-        (QLabel,            'manualLink'         ),
-        (QLabel,            'newFileLinkShortcut'),
-        (QLabel,            'openFileShortcut'   ),
-        (QLabel,            'poweredByKDELink'   ),
-        (QLabel,            'sourceCodeLink'     ),
-        (QLabel,            'supportKritaLink'   ),
-        (QLabel,            'userCommunityLink'  ),
-        (QPushButton,       'gettingStartedIcon' ),
-        (QPushButton,       'kdeIcon'            ),
-        (QPushButton,       'kritaWebsiteIcon'   ),
-        (QPushButton,       'sourceCodeIcon'     ),
-        (QPushButton,       'supportKritaIcon'   ),
-        (QPushButton,       'userCommunityIcon'  ),
-        (QPushButton,       'userManualIcon'     ),
-        (QWidget,           'widgetRight'        ),
-    ],
-}
+supportedKritaVersions = [
+    '5.1', # Tested with: Krita 5.1.5
+    '5.2', # Tested with: Krita 5.2.0 beta2
+    '5.3', # Tested with: Krita 5.3.0 prealpha
+]
+
+blocklistDefinitions = [
+    (['5.1', '5.2', '5.3'], [
+        # Community links
+        (QLabel,      'gettingStartedLink' ),
+        (QLabel,      'helpTitleLabel'     ),
+        (QLabel,      'kritaWebsiteLink'   ),
+        (QLabel,      'manualLink'         ),
+        (QLabel,      'poweredByKDELink'   ),
+        (QLabel,      'sourceCodeLink'     ),
+        (QLabel,      'supportKritaLink'   ),
+        (QLabel,      'userCommunityLink'  ),
+        (QPushButton, 'gettingStartedIcon' ),
+        (QPushButton, 'kdeIcon'            ),
+        (QPushButton, 'kritaWebsiteIcon'   ),
+        (QPushButton, 'sourceCodeIcon'     ),
+        (QPushButton, 'supportKritaIcon'   ),
+        (QPushButton, 'userCommunityIcon'  ),
+        (QPushButton, 'userManualIcon'     ),
+        # News panel
+        (QWidget,     'widgetRight'        ),
+        # Bottom text advert
+        (QLabel,      'labelSupportText'   ),
+    ]),
+    (['5.2', '5.3'], [
+        # Top banner advert
+        (QLabel,      'lblBanner'          ),
+        # Off-placed shortcut labels
+        (QLabel,      'newFileLinkShortcut'),
+        (QLabel,      'openFileShortcut'   ),
+    ]),
+]
 
 class KraAdblock(Extension):
     def __init__(self, parent):
         super().__init__(parent)
 
     def setup(self):
-        self.blockListVersion = re.search(r'(\d+\.\d+)\..*', Krita.instance().version()).group(1)
+        self.blocklistVersion = re.search(r'(\d+\.\d+)\..*', Krita.instance().version()).group(1)
 
-        if not self.blockListVersion in widgetBlockList:
+        if not self.blocklistVersion in supportedKritaVersions:
             print("kra-adblock: Unsupported Krita version")
             return
 
@@ -77,6 +61,8 @@ class KraAdblock(Extension):
 
     def blockAdsAllWindows(self):
         for window in Krita.instance().windows():
-            for widgetType, widgetName in widgetBlockList[self.blockListVersion]:
-                if widget := window.qwindow().findChild(widgetType, widgetName):
-                    widget.setVisible(False)
+            for blocklistVersions, blocklist in blocklistDefinitions:
+                if self.blocklistVersion in blocklistVersions:
+                    for widgetType, widgetName in blocklist:
+                        if widget := window.qwindow().findChild(widgetType, widgetName):
+                            widget.setVisible(False)
